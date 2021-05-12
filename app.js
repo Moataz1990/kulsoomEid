71b05ab54b217a96583345102aa1d36a3fa943fb
moataz <moataz-17@hotmail.com>
2021-05-12 23:57:02 +0500

    kulsoom

---
+++ app.js
@@ -0,0 +1,33 @@
+// const day = document.getElementById("day");
+// const hour = document.getElementById("hour");
+// const minute = document.getElementById("minute");
+// const second = document.getElementById("second");
+
+// function eid(){
+//     let sec = 1000;
+//     let min = sec * 60;
+//     let hr = min * 60;
+//     let dy = hr * 24 ;
+
+//     let countDown = new Date('5/13/2021 ,00:00:00').getTime();
+//     let today = new Date().getTime()
+//     let gap = countDown - today ;
+
+//     let d = Math.floor(gap / (dy))
+//     let h = Math.floor(gap % (dy) / (hr))
+//     let m = Math.floor(gap % (hr) / (min))
+//     let s = Math.floor(gap % (min) / (sec))
+    
+//     day.innerHTML = d;
+//     hour.innerHTML =h;
+//     minute.innerHTML =m;
+//     second.innerHTML=s;
+
+    
+// }
+
+
+// setInterval(() =>{
+//     eid();
+//     },1000)
+alert("CLICK ON THE COVER")
